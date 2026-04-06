ab4a22c1563b493f3393b8be2e1ea941d03ad7cc
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2026-04-05 19:14:03 -0500

    fix(help): enhance help command to prefer built-in help over aliases (#17979)

--- tests/repl/test_parser.rs
+++ tests/repl/test_parser.rs
@@ -457,6 +457,43 @@ fn percent_prefers_builtin_when_custom_shadows_name() -> Result {
         .expect_value_eq(false)
 }
 
+#[test]
+fn percent_help_prefers_builtin_when_alias_shadows_name() -> TestResult {
+    run_test_contains(
+        "alias cd = echo; %cd --help",
+        "Change the current working directory.",
+    )
+}
+
+#[test]
+fn help_percent_prefers_builtin_without_alias() -> TestResult {
+    run_test_contains("help %cd", "Change the current working directory.")
+}
+
+#[test]
+fn help_percent_prefers_builtin_when_alias_shadows_name() -> TestResult {
+    run_test_contains(
+        "alias cd = echo; help %cd",
+        "Change the current working directory.",
+    )
+}
+
+#[test]
+fn help_plain_keeps_alias_resolution_behavior() -> TestResult {
+    run_test_contains(
+        "alias cd = echo; help cd",
+        "Returns its arguments, ignoring the piped-in value.",
+    )
+}
+
+#[test]
+fn plain_help_keeps_alias_resolution_behavior() -> TestResult {
+    run_test_contains(
+        "alias cd = echo; cd --help",
+        "Returns its arguments, ignoring the piped-in value.",
+    )
+}
+
 #[rstest]
 #[case::unknown_command("%nu --version")]
 #[case::custom_command("def foo [] { 'ok' }; %foo")]
